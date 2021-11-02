349ab762c5140929987a03839dbc8ac97091e84b
User <nahkampf-1990@mail.ru>
2021-11-02 07:53:41 +0300

    Исправлена работа метода deleteTask Добавлена автоматическая генерация уникального ID Подключен фреймворк JUnit

---
+++ src/test/java/com/example/test_prodject_for_farzoom/controller/TaskControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.test_prodject_for_farzoom.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+}
---
+++ src/test/java/com/example/test_prodject_for_farzoom/repository/TaskRepositoryImplTest.java
@@ -0,0 +1,29 @@
+package com.example.test_prodject_for_farzoom.repository;
+
+import com.example.test_prodject_for_farzoom.models.Task;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskRepositoryImplTest {
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void getAllTaskSort() {
+    }
+}
